f138106d271465484cf506f4ed69ea8b368d4adf
DaveScott99 <davicalixto2077@gmail.com>
2024-12-31 15:33:11 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>TikeTeko</div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      },
+      colors: {
+        'toastSucess': 'rgba(46, 200, 98, .20)',
+        'toastError': 'rgba(228, 0, 35, .20)',
+        'toastMessage': ''
+      },
+      aspectRatio: {
+        '2/3': '2 / 3',
+        '4/3': '4/3'
+      }
+    },
+  },
+  plugins: [],
+}
